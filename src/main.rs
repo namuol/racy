b36b6d9cc6602f6ebcefe55a51f61c2a12570890
Louis Acresti <louis.acresti@gmail.com>
2020-05-30 09:25:28 -0700

    Sphere interesection testing

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ extern crate impl_ops;
 extern crate rayon;
 extern crate sdl2;
 
+use core::f64::consts::PI;
 use rayon::prelude::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 
 pub mod camera;
+pub mod line;
+pub mod ray;
 pub mod vector;
 
 use crate::camera::Camera;
+use crate::ray::Ray;
 use crate::vector::Vector;
 
 const SCREEN_WIDTH: u32 = 640;
@@ -39,17 +43,17 @@ pub fn main() {
         .unwrap();
 
     let _image_context = sdl2::image::init(sdl2::image::InitFlag::JPG);
-    let mut cam = Camera::new(
+    let cam = Camera::new(
         Vector {
-            x: 127.0,
-            y: 90.0,
-            z: 200.0,
+            x: 0.0,
+            y: 2.0, // meters
+            z: 0.0,
         },
         25.0,
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
     );
-    cam.set_angle(-std::f64::consts::PI);
+
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     let texture_creator = canvas.texture_creator();
 
@@ -67,6 +71,35 @@ pub fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut tick: f64 = 0.0;
+    let mut objects: Vec<Sphere> = vec![
+        Sphere {
+            color: RGB { r: 255, g: 0, b: 0 },
+            center: Vector {
+                x: -2.0,
+                y: 2.0,
+                z: 12.0,
+            },
+            radius: 1.0,
+        },
+        Sphere {
+            color: RGB { r: 0, g: 255, b: 0 },
+            center: Vector {
+                x: 0.0,
+                y: 2.0,
+                z: 12.0,
+            },
+            radius: 1.0,
+        },
+        Sphere {
+            color: RGB { r: 0, g: 0, b: 255 },
+            center: Vector {
+                x: 2.0,
+                y: 2.0,
+                z: 12.0,
+            },
+            radius: 1.0,
+        },
+    ];
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -78,31 +111,108 @@ pub fn main() {
                 _ => {}
             }
         }
+
         canvas.clear();
         screen_texture
             .with_lock(None, |mut screen, _size| {
-                render(&cam, &mut screen, tick);
+                render(&cam, &mut screen, &objects);
             })
             .unwrap();
         canvas
             .copy_ex(&screen_texture, None, None, 0.0, None, false, false)
             .unwrap();
         canvas.present();
-
+        let mut i = 0.0;
+        for mut sphere in &mut objects {
+            sphere.center.y = 2.0 + 0.5 * ((tick) * 0.01 + i * PI * 0.45).sin();
+            i += 1.0;
+        }
         tick += 1.0;
     }
 }
 
-fn render(cam: &Camera, screen: &mut [u8], _tick: f64) {
-    screen
-        .par_chunks_mut(4)
-        .enumerate()
-        .for_each(|(_i, pixel)| {
-            pixel[0] = 255;
-            pixel[1] = 0;
-            pixel[2] = 255;
-            pixel[3] = 255;
-        });
+fn render<S, I>(cam: &Camera, screen: &mut [u8], objects: &Vec<S>)
+where
+    I: Intersection,
+    S: IntersectsWithRay<I> + Sync + Colorful,
+{
+    let screen_width = cam.screen_width as usize;
+    screen.par_chunks_mut(4).enumerate().for_each(|(i, pixel)| {
+        let x = i % screen_width;
+        let y = i / screen_width;
+
+        let pixel_ray = cam.get_ray_from_uv(x, y);
+
+        match cast(&pixel_ray, &objects) {
+            None => (),
+            Some(color) => {
+                pixel[0] = color.r;
+                pixel[1] = color.g;
+                pixel[2] = color.b;
+                pixel[3] = 255;
+            }
+        }
+    });
+}
+
+fn cast<S, I>(ray: &Ray, objects: &Vec<S>) -> Option<RGB>
+where
+    I: Intersection,
+    S: IntersectsWithRay<I> + Colorful,
+{
+    for object in objects {
+        match object.intersects(ray) {
+            // For now we just return the first we intersect with:
+            Some(_) => return Some(object.color()),
+            None => continue,
+        }
+    }
+    None
+}
+
+trait Colorful {
+    fn color(&self) -> RGB;
+}
+
+trait Intersection {}
+trait IntersectsWithRay<I>
+where
+    I: Intersection,
+{
+    fn intersects(&self, ray: &Ray) -> Option<I>
+    where
+        I: Intersection;
+}
+
+struct Sphere {
+    pub center: Vector,
+    pub radius: f64,
+    pub color: RGB,
+}
+
+struct SphereIntersection {}
+impl Intersection for SphereIntersection {}
+
+impl Colorful for Sphere {
+    fn color(&self) -> RGB {
+        self.color
+    }
+}
+
+impl IntersectsWithRay<SphereIntersection> for Sphere {
+    fn intersects(&self, ray: &Ray) -> Option<SphereIntersection> {
+        let ray_origin_to_sphere_center = self.center - ray.origin;
+        let dist_to_sphere_center_squared = ray_origin_to_sphere_center.length_squared();
+        let dist_to_sphere_perpendicular_squared =
+            ray_origin_to_sphere_center.dot(&ray.direction).powi(2);
+
+        if (dist_to_sphere_center_squared - dist_to_sphere_perpendicular_squared)
+            < self.radius.powi(2)
+        {
+            return Some(SphereIntersection {});
+        }
+        None
+    }
 }
 
 #[derive(Clone, Copy)]
